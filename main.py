9802475db360b93096ae33d1fc6bfbb646b22de0
Kirill <nemollirik@gmail.com>
2021-12-17 19:41:08 +0500

    Добавлена простейшая стрельба, при попадании игрок исчезает

--- main.py
+++ main.py
@@ -70,6 +70,8 @@ class Board:
             self.board[x][y] = 0
             self.f[1] += 1
             self.board[x][y + 1] = 2
+        if first_player.direction == 0:
+            first_player.flip()
 
     def down1(self):
         x, y = self.f
@@ -84,6 +86,8 @@ class Board:
             self.board[x][y] = 0
             self.f[1] -= 1
             self.board[x][y - 1] = 2
+        if first_player.direction:
+            first_player.flip()
 
     def up2(self):
         x, y = self.s
@@ -98,6 +102,8 @@ class Board:
             self.board[x][y] = 0
             self.s[1] += 1
             self.board[x][y + 1] = 3
+        if second_player.direction == 0:
+            second_player.flip()
 
     def down2(self):
         x, y = self.s
@@ -112,6 +118,8 @@ class Board:
             self.board[x][y] = 0
             self.s[1] -= 1
             self.board[x][y - 1] = 3
+        if second_player.direction:
+            second_player.flip()
 
 
 class Sprite(pygame.sprite.Sprite):
@@ -120,13 +128,23 @@ class Sprite(pygame.sprite.Sprite):
         self.image = pygame.image.load(f"data/{file}")
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = x, y
+        self.mask = pygame.mask.from_surface(self.image)
+
+
+class Player(Sprite):
+    def __init__(self, *group, x=0, y=0, file="texture.png"):
+        super().__init__(*group, x=x, y=y, file=file)
+        self.direction = 1
 
     def update(self, x, y):
         self.rect.x, self.rect.y = x, y
 
+    def flip(self):
+        self.image = pygame.transform.flip(self.image, True, False)
+        self.direction = 0 if self.direction else 1
 
-class Player(Sprite):
-    pass
+    def die(self):
+        self.kill()
 
 
 class Wall(Sprite):
@@ -137,17 +155,44 @@ class Puddle(Sprite):
     pass
 
 
+class Bullet(Sprite):
+    def __init__(self, *group, x=0, y=0, file="texture.png", direction=None, enemy=None):
+        super().__init__(*group, x=x, y=y, file=file)
+        self.direction = direction
+        if direction == 0 or direction == 2:
+            self.image = pygame.transform.rotate(self.image, 90)
+        self.enemy = enemy
+
+    def update(self):
+        if pygame.sprite.spritecollideany(self, walls):
+            self.kill()
+        elif pygame.sprite.spritecollideany(self, self.enemy):
+            pygame.sprite.spritecollideany(self, self.enemy).die()
+            self.kill()
+        else:
+            if self.direction == 0:
+                self.rect.y -= 1
+            elif self.direction == 1:
+                self.rect.x += 1
+            elif self.direction == 2:
+                self.rect.y += 1
+            elif self.direction == 3:
+                self.rect.x -= 1
+
+
 pygame.init()
 screen = pygame.display.set_mode((1920, 1080))
 walls = pygame.sprite.Group()
-players = pygame.sprite.Group()
+first_players = pygame.sprite.Group()
+second_players = pygame.sprite.Group()
 water = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
 WIDTH, HEIGHT = 38, 21
 board = Board(WIDTH, HEIGHT)
 running = True
-first_player = Player(players, x=10, y=15, file="first_player.png")
-second_player = Player(players, x=1860, y=1015, file="second_player.png")
-second_player.image = pygame.transform.flip(second_player.image, True, False)
+first_player = Player(first_players, x=10, y=15, file="first_player.png")
+second_player = Player(second_players, x=1860, y=1015, file="second_player.png")
+second_player.flip()
 background = pygame.image.load("data/background.png")
 while running:
     for event in pygame.event.get():
@@ -170,9 +215,36 @@ while running:
                 board.down2()
             if event.key == pygame.K_LEFT:
                 board.left2()
+            if event.key == pygame.K_t:
+                Bullet(bullets, x=first_player.rect.x + 20, y=first_player.rect.y - 30, file="bullet.png", direction=0,
+                       enemy=second_players)
+            if event.key == pygame.K_h:
+                Bullet(bullets, x=first_player.rect.x + 50, y=first_player.rect.y + 20, file="bullet.png", direction=1,
+                       enemy=second_players)
+            if event.key == pygame.K_g:
+                Bullet(bullets, x=first_player.rect.x + 20, y=first_player.rect.y + 50, file="bullet.png", direction=2,
+                       enemy=second_players)
+            if event.key == pygame.K_f:
+                Bullet(bullets, x=first_player.rect.x - 30, y=first_player.rect.y + 20, file="bullet.png", direction=3,
+                       enemy=second_players)
+            if event.key == pygame.K_KP5:
+                Bullet(bullets, x=second_player.rect.x + 20, y=second_player.rect.y - 30, file="bullet.png",
+                       direction=0, enemy=first_players)
+            if event.key == pygame.K_KP3:
+                Bullet(bullets, x=second_player.rect.x + 50, y=second_player.rect.y + 20, file="bullet.png",
+                       direction=1, enemy=first_players)
+            if event.key == pygame.K_KP2:
+                Bullet(bullets, x=second_player.rect.x + 20, y=second_player.rect.y + 50, file="bullet.png",
+                       direction=2, enemy=first_players)
+            if event.key == pygame.K_KP1:
+                Bullet(bullets, x=second_player.rect.x - 30, y=second_player.rect.y + 20, file="bullet.png",
+                       direction=3, enemy=first_players)
     screen.blit(background, (0, 0))
     board.render()
-    players.draw(screen)
+    first_players.draw(screen)
+    second_players.draw(screen)
     walls.draw(screen)
     water.draw(screen)
+    bullets.update()
+    bullets.draw(screen)
     pygame.display.flip()
